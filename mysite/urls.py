3330871f8bce470f3a7ee5d2c77e761f127614fe
Martin Czerwinski <mrtn.czrwnski@gmail.com>
2026-03-18 11:31:15 -0600

    Redirect root URL to forum

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('django_simple_forum.urls')),
+    path('', RedirectView.as_view(url='/forum/')),
 ]
